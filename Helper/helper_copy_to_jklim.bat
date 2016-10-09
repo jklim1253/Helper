9573c43a8327d0ab3bfc859f3e191efe00c93672
jklim <jklim1253@gmail.com>
2016-10-09 11:22:03 +0900

    환경변수 jklim을 path_to_jklim에서 path_to_jklim\ 로 변경하면서 배치파일의 중복되는 \\를 \로 변경함.

--- Helper/helper_copy_to_jklim.bat
+++ Helper/helper_copy_to_jklim.bat
@@ -1,9 +1,9 @@
 : dll copy
-copy /y ..\Debug\Helper.dll %jklim%\dll\
-copy /y ..\Debug\Helper.pdb %jklim%\dll\
+copy /y ..\Debug\Helper.dll %jklim%dll\
+copy /y ..\Debug\Helper.pdb %jklim%dll\
 : lib copy
-copy /y ..\Debug\Helper.lib %jklim%\lib\
+copy /y ..\Debug\Helper.lib %jklim%lib\
 : header file copy
 : copy all header
-copy /y .\*.h %jklim%\include\
-copy /y .\*.hpp %jklim%\include\
+copy /y .\*.h %jklim%include\
+copy /y .\*.hpp %jklim%include\
